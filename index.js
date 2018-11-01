664165f3e734c328934e966814ad08b8e3fcafcc
Nikolaos Bellias <nikolaos.bellias@gmail.com>
2018-11-01 18:59:55 +0200

    Add Node.js server code

--- index.js
+++ index.js
@@ -8,3 +8,11 @@
 */
 
 const http = require('http');
+
+const httpServer = http.createServer((req,res)=>{
+    console.log("Request...");
+});
+
+httpServer.listen(3000, ()=>{
+    console.log("HTTP Server is listening on port 3000...");
+});
